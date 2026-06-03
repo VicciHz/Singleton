cb576f1441ef5d39b83b4e308c739662e3180814
Victor <victor.fox.hz@gmail.com>
2026-06-03 11:39:00 +0200

    Remove unused import for Scanner in Main.java

--- src/main/java/se/bytebase/Main.java
+++ src/main/java/se/bytebase/Main.java
@@ -1,7 +1,5 @@
 package se.bytebase;
 
-import java.util.Scanner;
-
 public class Main {
   public static void main(String[] args) {
     String url = "jdbc:postgresql://localhost:5432/singleton_db";
